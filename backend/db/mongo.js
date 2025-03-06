b14a65ce9e265ca7ec7fb6b44cb8b3a9167cc5d8
Arya Setia Pratama <aryasetiap.code@gmail.com>
2025-03-07 00:04:47 +0700

    update(code) db mongo

--- backend/db/mongo.js
+++ backend/db/mongo.js
@@ -2,9 +2,6 @@ console.log("MongoDB connection file executed");
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/mahamart", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://localhost:27017/mahamart");
 
 module.exports = mongoose;
